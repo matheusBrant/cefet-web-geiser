55f5acd8f5abc3d8621404e754db0adf7a64f351
Matheus Brant <matheus.brant@upflow.me>
2021-08-03 19:55:38 -0300

    ex3

--- server/app.js
+++ server/app.js
@@ -1,22 +1,28 @@
 // importação de dependência(s)
-
+const express = require('express');
+const fs = require('fs')
 
 // variáveis globais deste módulo
 const PORT = 3000
-const db = {}
+const app = express();
 
+let db = {}
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
 
-
-
-
+db = {
+  jogadores: JSON.parse(fs.readFileSync('server/data/jogadores.json')).players,
+  jogosPorJogador: JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json'))
+}
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 //app.set('view engine', '???qual-templating-engine???');
 //app.set('views', '???caminho-ate-pasta???');
 // dica: 2 linhas
+app.set('view engine','hbs')
+
+app.set('views','server/views')
 
 
 // EXERCÍCIO 2
@@ -24,7 +30,11 @@ const db = {}
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
-
+app.get('/', function(req, response) {
+    response.render('index',{
+      jogadores: db.jogadores
+    });
+});
 
 
 // EXERCÍCIO 3
@@ -33,11 +43,30 @@ const db = {}
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
 
+app.get('/jogador/:id', (req, res) => {
+  const dados = db.jogadores.players.find(f => f.steamid === req.params.id);  
+  
+  const result = {
+    steamid: dados.steamid,
+    personame: dados.personaname,
+    profileurl: dados.profileurl,
+    avatarmedium: dados.avatarmedium,
+    realname: dados.realname,
+    loccountrycode: dados.loccountrycode,
+
+  }
+  
+  res.render('jogador', result);
+});
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
+app.use(express.static('client'));
 
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
